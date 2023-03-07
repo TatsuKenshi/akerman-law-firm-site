674eb5868b391bab0aa5cb4c681442ad60e3c7be
TatsuKenshi <rockinbg@gmail.com>
2023-03-07 22:53:33 +0100

    moves flip cards to the Work Areas page

--- src/pages/WorkAreas.tsx
+++ src/pages/WorkAreas.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { withTranslation } from "react-i18next";
+import FlipCard from "../components/FlipCard";
+import { cardItems } from "../content/content";
 
-const WorkAreas = () => {
-  return <div>WorkAreas</div>;
+const WorkAreas = (t: any) => {
+  return (
+    <div className="flex">
+      {cardItems.map((item) => {
+        return <FlipCard key={item.id} {...item} />;
+      })}
+    </div>
+  );
 };
 
-export default WorkAreas;
+export default withTranslation()(WorkAreas);
