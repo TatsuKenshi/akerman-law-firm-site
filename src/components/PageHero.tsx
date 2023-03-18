b8e54c34a6d073a3a55afd9a0db1eccab3241bc4
TatsuKenshi <rockinbg@gmail.com>
2023-03-18 23:59:24 +0100

    adds progressive image loading to PageHero

--- src/components/PageHero.tsx
+++ src/components/PageHero.tsx
@@ -1,6 +1,8 @@
 import i18next from "i18next";
 import banner1 from "../assets/banner1.jpg";
 import { PageHeroPropsTypes } from "./types";
+import ProgressiveImage from "react-progressive-graceful-image";
+import placeholderSrc from "../assets/banner1Small.jpg";
 
 const PageHero = (props: PageHeroPropsTypes) => {
   const language = i18next.language;
@@ -36,11 +38,18 @@ const PageHero = (props: PageHeroPropsTypes) => {
         </div>
       </div>
       <div className="absolute top-0 right-0 block w-full h-full">
-        <img
-          alt="Corporate Office"
-          className="object-cover h-full min-w-full"
-          src={banner1}
-        />
+        <ProgressiveImage src={banner1} placeholder={placeholderSrc}>
+          {(src, loading) => (
+            <img
+              alt="Corporate Office"
+              className={`image${
+                loading ? " loading" : " loaded"
+              } object-cover h-full min-w-full`}
+              // className="object-cover h-full min-w-full"
+              src={src}
+            />
+          )}
+        </ProgressiveImage>
       </div>
     </div>
   );
