9ad7decafc6346563220004a829e5e69b91f2299
TatsuKenshi <rockinbg@gmail.com>
2023-03-23 18:54:27 +0100

    adds the DropDownMenu component

---
+++ src/components/DropDownMenu.tsx
@@ -0,0 +1,11 @@
+// concept should be similar to the filters section in the comfy typescript project
+
+const DropDownMenu = () => {
+  return (
+    <section>
+      <h1>DropDownMenu</h1>
+    </section>
+  );
+};
+
+export default DropDownMenu;
