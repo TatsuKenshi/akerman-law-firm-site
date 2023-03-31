de3626db4390f1fe1c75a7c8eb5ff8ffeef53e62
TatsuKenshi <rockinbg@gmail.com>
2023-03-31 15:24:18 +0200

    removes the dropdown menu

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,7 +5,6 @@ import i18n from "../translation";
 
 const PageHero = React.lazy(() => import("../components/PageHero"));
 const ContactForm = React.lazy(() => import("../components/ContactForm"));
-const HomeHero = React.lazy(() => import("../components/HomeHero"));
 
 const Contact = ({ t }: any) => {
   // page title customization and localization
@@ -19,7 +18,6 @@ const Contact = ({ t }: any) => {
   return (
     <main>
       <PageHero srTitle="" srContent="" engTitle="" engContent="" />
-      {/* <HomeHero srTitle="" srContent="" engTitle="" engContent="" /> */}
 
       {/* title div */}
       <div className="mt-8 text-center mx-4">
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -9,8 +9,6 @@ import Company from "../assets/Company.jpg";
 import CompanySmall from "../assets/CompanySmall.jpg";
 import Enforcement1 from "../assets/Enforcement1.jpg";
 import Enforcement1Small from "../assets/Enforcement1Small.jpg";
-import Enforcement3 from "../assets/Enforcement3.jpg";
-import Enforcement3Small from "../assets/Enforcement3Small.jpg";
 import FamilyAndInheritance1 from "../assets/FamilyAndInheritance1.jpg";
 import FamilyAndInheritance1Small from "../assets/FamilyAndInheritance1Small.jpg";
 import Labour1 from "../assets/Labour1.jpg";
@@ -22,8 +20,6 @@ import RepresentingClientsSmall from "../assets/RepresentingClientsSmall.jpg";
 
 const PageHero = React.lazy(() => import("../components/PageHero"));
 const RevealCard = React.lazy(() => import("../components/RevealCard"));
-const DropDownMenu = React.lazy(() => import("../components/DropDownMenu"));
-const HomeHero = React.lazy(() => import("../components/HomeHero"));
 
 const Services = ({ t }: any) => {
   const {
@@ -54,7 +50,6 @@ const Services = ({ t }: any) => {
   return (
     <main>
       <PageHero srTitle="" srContent="" engTitle="" engContent="" />
-      {/* <HomeHero srTitle="" srContent="" engTitle="" engContent="" /> */}
 
       {/* title div */}
       <div className="mt-8 text-center mx-4">
@@ -63,9 +58,6 @@ const Services = ({ t }: any) => {
         </h1>
         <div className="h-[4px] w-11/12 max-w-[270px] bg-yellow-700 bg-opacity-70 mx-auto mt-2"></div>
       </div>
-
-      {/* Dropdown menu section */}
-      <DropDownMenu />
 
       {/* reveal card section */}
       <section>
