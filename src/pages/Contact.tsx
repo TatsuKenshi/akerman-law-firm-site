2a4deb4704ae26df32e3a67acb54c6a4d6daf62f
TatsuKenshi <rockinbg@gmail.com>
2023-04-12 17:16:40 +0200

    adds the Disclaimer section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,6 +5,7 @@ import i18n from "../translation";
 
 const PageHero = React.lazy(() => import("../components/PageHero"));
 const ContactForm = React.lazy(() => import("../components/ContactForm"));
+const Disclaimer = React.lazy(() => import("../components/Disclaimer"));
 
 const Contact = ({ t }: any) => {
   // page title customization and localization
@@ -24,6 +25,9 @@ const Contact = ({ t }: any) => {
         <h1 className="text-4xl font-bold text-sky-900">{t("ContactTitle")}</h1>
         <div className="h-[4px] w-11/12 max-w-[270px] bg-yellow-700 bg-opacity-70 mx-auto mt-2"></div>
       </div>
+
+      {/* disclaimer section */}
+      <Disclaimer />
 
       {/* form section */}
       <ContactForm />
