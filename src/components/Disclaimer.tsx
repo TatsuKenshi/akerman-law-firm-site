91b637446f65de216047ae68949360cff12ec3c6
TatsuKenshi <rockinbg@gmail.com>
2023-04-15 23:14:59 +0200

    adds placeholder text to the Disclaimer component

--- src/components/Disclaimer.tsx
+++ src/components/Disclaimer.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Disclaimer = () => {
-  return <div>Disclaimer</div>;
+  return (
+    <div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam
+        nesciunt ab laborum ad dolor quas eligendi distinctio aspernatur maxime
+        dolorum explicabo error incidunt, iure dolores vel? Nisi dolores
+        suscipit soluta eligendi culpa quidem odit doloremque unde ea id
+        cupiditate, officia corrupti quia voluptatum explicabo porro iusto
+        eveniet molestias nostrum? Consequatur?
+      </p>
+    </div>
+  );
 };
 
 export default Disclaimer;
