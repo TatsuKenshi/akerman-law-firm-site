db60e44026a26dd973e1fba81a9eebedf8729d7d
TatsuKenshi <rockinbg@gmail.com>
2023-04-21 19:05:15 +0200

    new tasks

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ const Contact = React.lazy(() => import("./pages/Contact"));
 const Error = React.lazy(() => import("./pages/Error"));
 const Terms = React.lazy(() => import("./pages/Terms"));
 
+// Sakerman SaraSara0101!
+
 function App() {
   return (
     <Routes>
