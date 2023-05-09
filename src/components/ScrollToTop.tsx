859d1732401d3793f1b4e9db2a270cd0e74ca856
TatsuKenshi <rockinbg@gmail.com>
2023-05-09 18:42:57 +0200

    lifts the scroll to top button to be above the cookie banner

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -25,7 +25,7 @@ const ScrollToTop = () => {
   }, []);
 
   return (
-    <section className="fixed bottom-4 right-2 md:right-4 lg:right-8 z-[50]">
+    <section className="fixed bottom-16 right-2 md:right-4 lg:right-8 z-[50]">
       <div className="mb-2">
         <a href="tel:+38166426624">
           <button className="p-2 rounded-full bg-white border-4 border-sky-900">
