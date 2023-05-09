ec803f0f495d4536057858f0eb54f95f92de299c
TatsuKenshi <rockinbg@gmail.com>
2023-05-09 19:36:08 +0200

    minor css fixes home page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,11 +4,11 @@ const About = ({ t }: any) => {
   return (
     <section className="mt-8 mb-16">
       <div className="flex max-w-[1200px] mx-auto flex-wrap mt-4 px-8 pt-4">
-        <h3 className="text-2xl font-bold text-sky-900">
+        <h3 className="text-2xl font-bold text-sky-900 w-full">
           {t("AboutCompanyName")}
         </h3>
         <p>{t("AboutCompanyNameParagraph")}</p>
-        <h3 className="text-2xl font-bold text-sky-900 mt-8">
+        <h3 className="text-2xl font-bold text-sky-900 mt-8 w-full">
           {t("AboutAreas")}
         </h3>
         <p>{t("AboutAreasParagraph")}</p>
